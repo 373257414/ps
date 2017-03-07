267ed85e5c198c51245b2c726d30ff23b9630cea
liuyang <373257414@qq.com>
2017-03-07 16:29:07 +0800

    getAllCategory

--- src/main/java/com/ycw/photosystem/controller/CategoryController.java
+++ src/main/java/com/ycw/photosystem/controller/CategoryController.java
@@ -4,6 +4,7 @@ import com.ycw.photosystem.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class CategoryController {
     CategoryService categoryService;
 
     @RequestMapping("getAllCategory")
+    @ResponseBody
     public List getAllCategory() {
         return categoryService.findAll();
     }
